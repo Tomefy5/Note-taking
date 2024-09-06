8e2cd915e7e80e57e878c137e95a779f5e050eb4
tomefy andry tsiresy <tomefyandrytsiresy@gmail.com>
2024-09-06 12:32:36 +0300

    Correct pathname

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import './bootstrap-5.3.3-dist/css/bootstrap.min.css'
-import './fontawesome-free-6.0.0-beta3-web/css/all.min.css'
+import './bootstrap-5.3.3-dist/css/bootstrap.min.css';
+import './fontawesome-free-6.0.0-beta3-web/css/all.min.css';
 import './App.css';
 import { Component } from 'react';
 import { Route, Routes, useLocation } from 'react-router-dom';
@@ -10,86 +10,89 @@ import NoteDetailWrapper from './Components/NoteDetailWrapper';
 
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
-    const savedNotes = localStorage.getItem("notes")
+    const savedNotes = localStorage.getItem('notes');
     this.state = {
       notes: savedNotes ? JSON.parse(savedNotes) : [],
       functions: {
         addNewNote: this.addNewNote,
         saveNoteHandler: this.saveNoteHandler,
-        deleteHandler: this.deleteHandler
-      }
-    }
+        deleteHandler: this.deleteHandler,
+      },
+    };
   }
 
   componentDidUpdate(prevProps, prevState) {
     if (prevState.notes !== this.state.notes) {
-      localStorage.setItem("notes", JSON.stringify(this.state.notes))
+      localStorage.setItem('notes', JSON.stringify(this.state.notes));
     }
   }
 
   addNewNote = (e) => {
-    e.preventDefault()
-    const newNoteTitle = prompt("Enter the note's title:")
+    e.preventDefault();
+    const newNoteTitle = prompt("Enter the note's title:");
 
-    if (newNoteTitle.trim() !== "") {
+    if (newNoteTitle.trim() !== '') {
       const newNote = {
         id: this.state.notes.length + 1,
         title: newNoteTitle,
-        body: "",
-        favorite: false
-      }
-      this.setState({
-        notes: [...this.state.notes, newNote]
-      })
+        body: '',
+        favorite: false,
+      };
+      this.setState((prevState) => ({
+        notes: [...prevState.notes, newNote],
+      }));
     }
-  }
+  };
 
   saveNoteHandler = (noteId) => {
-    const textarea = document.querySelector("textarea")
+    const textarea = document.querySelector('textarea');
 
     this.setState((prevState) => ({
-      notes: prevState.notes.map((note) => {
-      return  note.id === noteId 
-              ? { ...note,  body: textarea.value} 
-              : note
-      })
-    }))
-  }
+      notes: prevState.notes.map((note) =>
+        note.id === noteId ? { ...note, body: textarea.value } : note
+      ),
+    }));
+  };
 
   deleteHandler = (noteId) => {
     this.setState((prevState) => ({
-      notes: prevState.notes.filter((todo) => (todo.id !== noteId))
-    }))
-  }
+      notes: prevState.notes.filter((note) => note.id !== noteId),
+    }));
+  };
+
   render() {
-    const isDetailPage = this.props.location.pathname.startsWith('/note/')
+    const isDetailPage = this.props.location.pathname.startsWith('/note/');
     return (
       <div className="container card mt-5 d-flex align-items-center p-0 app">
-          {!isDetailPage && <h1 className="title display-6 mt-3">Note Taking</h1>}
-          {!isDetailPage && <SearchInput />}
-          {!isDetailPage && <MenuBar functions={this.state.functions} />}
-          <Routes>
-            <Route  
-                  path='/' 
-                  element={<NotesListe notes={this.state.notes} deleteHandler={this.deleteHandler} />}
-            />
-            <Route 
-                  path='/note/:id'
-                  element={<NoteDetailWrapper notes={this.state.notes} functions={this.state.functions}/>} 
-            />
-          </Routes>
+        {!isDetailPage && <h1 className="title display-6 mt-3">Note Taking</h1>}
+        {!isDetailPage && <SearchInput />}
+        {!isDetailPage && <MenuBar functions={this.state.functions} />}
+        <Routes>
+          <Route
+            path="/Note-taking"
+            element={
+              <NotesListe notes={this.state.notes} />
+            }
+          />
+          <Route
+            path="/note/:id"
+            element={
+              <NoteDetailWrapper notes={this.state.notes} functions={this.state.functions} />
+            }
+          />
+        </Routes>
       </div>
-    )
+    );
   }
 }
 
 function displayWithRouter(Component) {
-  function ComponentDisplayed(props) {
-    const location = useLocation()
-    return <Component {...props} location={location} />
-  }
-  return ComponentDisplayed
+  return function ComponentDisplayed(props) {
+    const location = useLocation();
+    return <Component {...props} location={location} />;
+  };
 }
+
 export default displayWithRouter(App);
